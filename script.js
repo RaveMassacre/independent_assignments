f4d9c64f92c5a60744a2217d951a3a1618077a63
Ilya Nosov <ravemassacre22@gmail.com>
2021-08-19 22:59:39 +0500

    feat(script.js): added input cleaning

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 
 const addParagraph = function () {
   const input = document.querySelector(`input`);
-  if (input.value.trim().length > 0) {
+  if (input.value.trim().length > 0 && input.type === `text`) {
     const li = document.createElement(`li`);
     li.innerText = input.value.trim();
     document.querySelector(`ul`).appendChild(li);
+    input.value = ` `;
   }
-
 };
+
 
 const button = document.querySelector(`button`);
 button.addEventListener('click', addParagraph);
